ffe88d7c81e260070a6c5a0006eae2fe87cf2b68
Robert Lancaster <rlancaste@gmail.com>
2019-03-09 16:56:23 -0500

    Adding copyright notice

--- make_Yearly_Text_Archive.py
+++ make_Yearly_Text_Archive.py
@@ -1,4 +1,27 @@
 #!/usr/local/bin/python
+'''
+Yahoo-Groups-Archiver, Text Archive Script Copyright 2019 Robert Lancaster and others
+
+YahooGroups-Archiver, a simple python script that allows for all
+messages in a public Yahoo Group to be archived.
+
+The Text Archive Script allows you to take the downloaded json documents
+and turn them yearly archives of emails sorted into text documents.
+Note that the archive-group.py script must be run first.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import email
 import HTMLParser
 import json
